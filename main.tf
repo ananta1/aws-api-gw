5474765f9edb4c0c802a70c90ac8f3ec234a516c
ananta1 <ananta1@users.noreply.github.com>
2025-01-20 01:14:22 -0600

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,1 +1,11 @@
+provider "aws" {
+  region = "us-east-1"
+}
 
+resource "aws_api_gateway_rest_api" "api" {
+  name        = "APIGWithLambda"
+  description = "API Gateway with Lambda Integration"
+  endpoint_configuration {
+    types = ["REGIONAL"]
+  }
+}
